4bc62ca9c397044534bc8470eccb185038641439
Edouard Rabany <edouard.rabany@piksel.com>
2015-11-10 15:21:29 +0100

    La note [Version 0.1]
    Signed-off-by: Edouard Rabany <edouard.rabany@piksel.com>

---
+++ scripts/app.js
@@ -0,0 +1,51 @@
+/*
+Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
+This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+Code distributed by Google as part of the polymer project is also
+subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+*/
+
+(function(document) {
+  'use strict';
+
+  // Grab a reference to our auto-binding template
+  // and give it some initial binding values
+  // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
+  var app = document.querySelector('#app');
+
+  // Let the user know that offline caching has worked and their
+  // app is available offline
+  app.displayInstalledToast = function() {
+    document.querySelector('#caching-complete').show();
+  };
+
+})(document);
+
+function uuid(){
+    var d = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (d + Math.random()*16)%16 | 0;
+        d = Math.floor(d/16);
+        return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+    });
+    return uuid;
+}
+
+Object.size = function(obj) {
+    var size = 0, key;
+    for (key in obj) {
+        if (obj.hasOwnProperty(key)) size++;
+    }
+    return size;
+};
+
+function selectPlace(e) {
+
+  if($(e.target) && $(e.target).closest('.selectItem').find('.key')) {
+    var keyPlace = $(e.target).closest('.selectItem').find('.key').text();
+    $('main-view').get(0).selectPlace(keyPlace);
+  }
+
+}
